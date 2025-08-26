07e94fb6300c739a45bd78a7d1f0b29055da717d
Bhaaraadwaj <104763004+Bhaaraadwaj@users.noreply.github.com>
2025-08-26 18:28:08 +0530

    Aug 26: Daily code challenge

--- 678/validparan.cpp
+++ 678/validparan.cpp
@@ -4,36 +4,51 @@
 using namespace std;
 bool checkValidString(string s)
 {
-    int low=0;
-    int high=0;
-    for(int i=0;i<s.length();i++)
+    // ( minOpen ++ 
+    // ( maxOpen ++
+    // ) minOpen--
+    // ) maxOpen--
+    // * minOpen-- if I get * ) or '' 
+    // * maxOpen++ if I get * (
+
+    // *** maxOpen=3
+    // minopen -3 = 0 
+    // *) minOpen=-1 maxOpen=1
+    // minOpen=-2
+    // maxOpen=0
+
+    // (*()
+
+
+    int minOpen=0;
+    int maxOpen=0;
+
+    for(char c:s)
     {
-        if(s[i]=='(')
+        if(c=='(')
         {
-            low++;
-            high++;
+            minOpen++;maxOpen++;
         }
-        else if(s[i]==')')
+        else if(c==')')
         {
-            low--;
-            high--;
+            minOpen--;maxOpen--;
         }
-        else
+        else 
         {
-            low--;
-            high++;
+            minOpen--;
+            maxOpen++;
         }
-        if(high<0)
+        if(maxOpen<0)
         {
             return false;
         }
-        low=max(low,0);
+        minOpen=max(0,minOpen);
     }
-    return low==0? true:false;
+    return minOpen==0;
 }
 int main()
 {
     string s="(*)";
-    cout<<(checkValidString(s)==true ? "Valid":"InValid");
+    cout<<(checkValidString(s)==true ? "Valid":"InValid")<<endl;
     return 0;
 }
